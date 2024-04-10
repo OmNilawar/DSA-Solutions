1e8055ddd0e819b1ff23457bece4880fba492e48
SOULEDSPY-G <omkar.nilawar.cs@ghrcem.raisoni.net>
2024-04-10 12:10:21 +0530

    forth comment

---
+++ Reveal_cards_in_Increasing_order.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> deckRevealedIncreasing(vector<int>& deck) {
+        sort(deck.begin(), deck.end());
+        int n = deck.size();
+        
+        vector<int> ans(n);
+        queue<int> indices;
+        
+        
+        for (int i = 0; i < n; ++i)
+            indices.push(i);
+        
+        for (int card : deck) {
+            ans[indices.front()] = card;
+            indices.pop();
+            if (!indices.empty()) {
+                indices.push(indices.front());
+                indices.pop();
+            }
+        }
+        
+        return ans;
+    }
+};
