b2fbfd555fb70d9008a98afa5d8ca081813b33dd
SOULEDSPY-G <omkar.nilawar.cs@ghrcem.raisoni.net>
2024-04-12 18:51:32 +0530

    #

---
+++ LinkedList/Reverse_Linked_List.cpp
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/reverse-linked-list/description/
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head==NULL || head->next==NULL) return head;
+        ListNode* cur=head, *Next, *prev=NULL;
+        while(cur!=NULL){
+            Next=cur->next;
+            cur->next=prev;
+            prev=cur;
+            cur=Next;
+        }
+        return prev;
+    }
+};
