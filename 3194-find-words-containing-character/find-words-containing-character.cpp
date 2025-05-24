ea6bc98128a17b3721f5a1a55a0bfe1943006dca
Omkar Nilawar <104561064+OmNilawar@users.noreply.github.com>
2025-05-24 22:06:00 +0530

    Time: 4 ms (15.60%) | Memory: 33.8 MB (12.10%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.cpp
+++ 3194-find-words-containing-character/find-words-containing-character.cpp
@@ -7,6 +7,7 @@ public:
         for(int i = 0;i < n;i++)
         {
             string temp = words[i];
+            sort(temp.begin(),temp.end());
             int m = temp.length();
 
             for(int j = 0;j < m;j++)
@@ -16,6 +17,9 @@ public:
                     ans.push_back(i);
                     break;
                 }
+
+                if(temp[j] > x)
+                    break;
             }
         }
 
