ac6e4b9985c8a4b238afc7d4566e96ca8137ff73
Omkar Nilawar <104561064+OmNilawar@users.noreply.github.com>
2025-07-20 11:58:26 +0530

    Time: 3 ms (9.58%) | Memory: 19.5 MB (31.79%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -2,18 +2,19 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
         int n = numbers.size();
-        unordered_map<int,int> mpp;
+        int l = 0;
+        int r = n-1;
 
-        for(int i = 0;i < n;i++)
+        while(l < r)
         {
-            mpp[numbers[i]] = i;
-        }
-
-        for(int i = 0;i < n;i++)
-        {
-            int diff = target - numbers[i];
-            if(mpp.find(diff) != mpp.end() && mpp[diff] != i)
-                return {i+1,mpp[diff]+1};
+            int sum = numbers[l] + numbers[r];
+            if(sum == target)
+                return {l+1,r+1};
+            
+            else if(sum > target)
+                r--;
+            else
+                l++;
         }
 
         return {-1,-1};
