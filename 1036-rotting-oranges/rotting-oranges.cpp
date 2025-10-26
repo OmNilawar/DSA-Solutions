564254d56b6edb8f2462c22b24aec739ab828238
Omkar Nilawar <104561064+OmNilawar@users.noreply.github.com>
2025-10-26 09:47:56 +0530

    Time: 3 ms (29.11%) | Memory: 16.9 MB (59.86%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,40 +1,55 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& grid, vector<vector<int>>& time, int i, int j, int t) {
-        int n = grid.size(), m = grid[0].size();
-        if(i<0 || j<0 || i>=n || j>=m || grid[i][j]==0) return;
-        if(grid[i][j]==1 && (time[i][j]==0 || time[i][j]>t))
-            time[i][j] = t;
-        else if(grid[i][j]==1 && time[i][j]<=t) return;
+    void traverse(vector<vector<int>>& grid, vector<vector<int>>& result, int i, int j, int k)
+    {
+        if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] == 0)
+            return ;
+        
+        if (result[i][j] != 0 && result[i][j] <= k)
+            return;
+
+        if(grid[i][j] == 1)
+            if(result[i][j] == 0 || result[i][j] > k)
+                result[i][j] = k;
 
         int temp = grid[i][j];
         grid[i][j] = 0;
 
-        dfs(grid, time, i+1, j, t+1);
-        dfs(grid, time, i-1, j, t+1);
-        dfs(grid, time, i, j+1, t+1);
-        dfs(grid, time, i, j-1, t+1);
+        traverse(grid, result, i+1, j, k+1);
+        traverse(grid, result, i-1, j, k+1);
+        traverse(grid, result, i, j+1, k+1);
+        traverse(grid, result, i, j-1, k+1);
 
         grid[i][j] = temp;
     }
-
     int orangesRotting(vector<vector<int>>& grid) {
-        int n = grid.size(), m = grid[0].size();
-        vector<vector<int>> time(n, vector<int>(m,0));
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> result(n, vector<int>(m,0));
 
-        for(int i=0;i<n;i++)
-            for(int j=0;j<m;j++)
-                if(grid[i][j]==2)
-                    dfs(grid, time, i, j, 0);
+        for(int i = 0;i < n;i++)
+        {
+            for(int j = 0;j < m;j++)
+            {
+                if(grid[i][j] == 2)
+                    traverse(grid, result, i, j, 0);
+            }
+        }
 
         int maxi = 0;
-        for(int i=0;i<n;i++)
-            for(int j=0;j<m;j++)
-                if(grid[i][j]==1) {
-                    if(time[i][j]==0) return -1;
-                    maxi = max(maxi, time[i][j]);
-                }
 
+        for(int i = 0;i < n;i++)
+        {
+            for(int j = 0;j < m;j++)
+            {
+                if(grid[i][j] == 1 && result[i][j] == 0)
+                {
+                    return -1;
+                }
+                maxi = max(maxi, result[i][j]);
+            }
+        }
+        
         return maxi;
     }
 };
