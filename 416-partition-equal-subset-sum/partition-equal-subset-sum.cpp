bdb686e95010050ff0f3a4b132685a78a670842e
Omkar Nilawar <104561064+OmNilawar@users.noreply.github.com>
2026-05-06 10:01:01 +0530

    Time: 130 ms (51.91%) | Memory: 17.5 MB (57.20%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool traverse(vector<int>& nums, int sum)
+    {
+        int n = nums.size();
+        vector<vector<bool>> dp(n+1, vector<bool> (sum+1, false));
+        
+        for(int i = 0;i <= n;i++)
+            dp[i][0] = true;
+
+        for(int i = 1;i <= n;i++)
+        {
+            for(int j = 1;j <= sum;j++)
+            {
+                if(nums[i-1] <= j)
+                    dp[i][j] = dp[i-1][j - nums[i-1]] || dp[i-1][j];
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+
+        return dp[n][sum];
+    }
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+        int total = 0;
+
+        for(int i = 0;i < n;i++) total += nums[i];
+    
+        if(total%2 != 0)
+            return false;
+        
+        return traverse(nums, total/2);
+    }
+};
