e01de271388889a5344dfc7341ef0804348cb523
Omkar Nilawar <104561064+OmNilawar@users.noreply.github.com>
2026-09-19 21:18:08 +0530

    Time: 0 ms (100.00%) | Memory: 25.7 MB (7.69%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -8,15 +8,15 @@ public:
         while(l < r)
         {
             int sum = numbers[l] + numbers[r];
+
             if(sum == target)
-                return {l+1,r+1};
-            
+                return {l+1, r+1};
             else if(sum > target)
                 r--;
             else
                 l++;
         }
 
-        return {-1,-1};
+        return {-1, -1};
     }
 };
